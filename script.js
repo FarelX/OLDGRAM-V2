4ae31552d6626b085cb4ab86e90f27f1c513d644
Farel Rizky <147903975+FarelX@users.noreply.github.com>
2026-02-12 00:20:34 +0700

    Refactor render loop and implement like button logic

--- script.js
+++ script.js
@@ -28,3 +28,46 @@ const posts = [
     likes: 152,
   },
 ];
+
+let feeds = document.getElementById("section");
+
+function render() {
+  html = "";
+  for (i = 0; i < posts.length; i++) {
+    html += `<div class="container">
+        <div class="posts-section">
+          <div class="profile-details">
+            <img src="${posts[i].avatar}" alt="" class="user-avatar"/>
+            <p>
+              <span class="bold">${posts[i].name}</span
+              ><span class="location">${posts[i].location}</span>
+            </p>
+          </div>
+          <img src="${posts[i].post}" alt="" class="post-img" />
+          <div class="interact-section">
+            <img src="images/icon-heart.png" alt="" class="icons" id="like-btn-${i}"/>
+            <img src="images/icon-comment.png" alt="" class="icons" />
+            <img src="images/icon-dm.png" alt="" class="icons" />
+            <p class="like-counts"><span class="bold" id="post-likes-${i}">${posts[i].likes} likes</span></p>
+            <p class="comments">
+              <span class="bold">${posts[i].username}</span> ${posts[i].comment}
+            </p>
+          </div>
+        </div>
+      </div>
+  `;
+    feeds.innerHTML = html;
+  }
+  for (let i = 0; i < posts.length; i++) {
+    document
+      .getElementById(`like-btn-${i}`)
+      .addEventListener("dblclick", function () {
+        posts[i].likes++;
+        document.getElementById(`post-likes-${i}`).textContent =
+          posts[i].likes + " likes";
+        document.getElementById(`like-btn-${i}`).src =
+          "images/icon-heart-liked.png";
+      });
+  }
+}
+render();
